ef1c6ff328e26d97dd66472d09b4ca0ebfcf34da
Zahid773 <zahid773wale@gmail.com>
2025-09-03 14:12:02 +0500

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "",
+SESSION_ID: process.env.SESSION_ID || "SHABAN-MD~eyJub2lzZUtleSI6eyJwcml2YXRlIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoiT0NDMUxTeHp5UXM2NjlYWkhMV0lJb0psaTVHNEx0V3FTMytIeGVvV3pIbz0ifSwicHVibGljIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoidXpXQUNuSGxQTlg0YTBEUlNMYVZ5V0xnOHNqcDQ3dGw2d1ZCK1VIWjZ5UT0ifX0sInBhaXJpbmdFcGhlbWVyYWxLZXlQYWlyIjp7InByaXZhdGUiOnsidHlwZSI6IkJ1ZmZlciIsImRhdGEiOiJzQkcwZTJoUXRYaWFtOG80dG93MkhGK2I5WFpjcTZIdUhIREgzMG1oNkdRPSJ9LCJwdWJsaWMiOnsidHlwZSI6IkJ1ZmZlciIsImRhdGEiOiI3Y0hJR0pmczZqZFczQVpjbHZhd2FDa1BMUUdrbmtFYnR3QnB4THppREg4PSJ9fSwic2lnbmVkSWRlbnRpdHlLZXkiOnsicHJpdmF0ZSI6eyJ0eXBlIjoiQnVmZmVyIiwiZGF0YSI6IitKTEVoTDBETEtWa3hSTUpMRG1PaHF4UjZHT2J4VHVRbWVTRjZEYlBxblk9In0sInB1YmxpYyI6eyJ0eXBlIjoiQnVmZmVyIiwiZGF0YSI6IjlVaFVnd2ZSNGFYQWRsajA4emRwZ1hhYWhUWGI5aWw4WCtrb3FHVGNSRlE9In19LCJzaWduZWRQcmVLZXkiOnsia2V5UGFpciI6eyJwcml2YXRlIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoiV0JvT1lhK29vdDVSQktrWFV2L2pKbUZZQmNRNHVPMkxmbWh1YWlMVENrTT0ifSwicHVibGljIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoiM1FlQmRBRlFxRWlaNVQycE51SXVudjZVcGU5MFRmQnFQYVZUMUxxRnZXdz0ifX0sInNpZ25hdHVyZSI6eyJ0eXBlIjoiQnVmZmVyIiwiZGF0YSI6ImJsVFV2RUtoMEdtVjJlTEFOU1A0azNnZDFXaG82QVhKU2padWgwK3puTFJnZUN6VUVHSmZaa0swQXBDS2thd3RGdHdLVCtpc1N0M3Bac3pFOTczeERRPT0ifSwia2V5SWQiOjF9LCJyZWdpc3RyYXRpb25JZCI6MTU5LCJhZHZTZWNyZXRLZXkiOiJtZUNhU3dVVmQxV3Y1ZklGTllyMFJlUlR2clExQXhxYlFuUDUvQ2JWL28wPSIsInByb2Nlc3NlZEhpc3RvcnlNZXNzYWdlcyI6W10sIm5leHRQcmVLZXlJZCI6MzEsImZpcnN0VW51cGxvYWRlZFByZUtleUlkIjozMSwiYWNjb3VudFN5bmNDb3VudGVyIjowLCJhY2NvdW50U2V0dGluZ3MiOnsidW5hcmNoaXZlQ2hhdHMiOmZhbHNlfSwicmVnaXN0ZXJlZCI6dHJ1ZSwicGFpcmluZ0NvZGUiOiIzNEgxUTQ0NCIsIm1lIjp7ImlkIjoiOTIzMjQ4MTYxNzczOjIwQHMud2hhdHNhcHAubmV0IiwibmFtZSI6IlphaGlkIiwibGlkIjoiODkxMjA5MjM3MTM3MDk6MjBAbGlkIn0sImFjY291bnQiOnsiZGV0YWlscyI6IkNLKzdrY2dFRU9YeDNzVUdHQUVnQUNnQSIsImFjY291bnRTaWduYXR1cmVLZXkiOiJWaElKNE94aTdqYTdSTHE2VEJiOGMzSWwySHZoYitsQTIwbk1uRWN4eEg0PSIsImFjY291bnRTaWduYXR1cmUiOiJCQSt4dmU3ZWM0ZGhqU2ZaWmlvL2Iza2g1SU9rTFdoY1MyYjVMR1F2QkVleHM5cWp0T29QSDhSak9OYlFkUThlNXhMTXlNem8xQ05FL3lhcWtqeUxBdz09IiwiZGV2aWNlU2lnbmF0dXJlIjoiL3dNQzNTeDcxR3BqcEtyTUJiVzhXUWd1Q1NGMmVXN0RxWlF4WktpeTRLaUlHTVE1d0o1elFoOFhmVzgzUWJmZzJCdXQ0Z3MvNFp2RkxmTitwV0V3QVE9PSJ9LCJzaWduYWxJZGVudGl0aWVzIjpbeyJpZGVudGlmaWVyIjp7Im5hbWUiOiI5MjMyNDgxNjE3NzM6MjBAcy53aGF0c2FwcC5uZXQiLCJkZXZpY2VJZCI6MH0sImlkZW50aWZpZXJLZXkiOnsidHlwZSI6IkJ1ZmZlciIsImRhdGEiOiJCVllTQ2VEc1l1NDJ1MFM2dWt3Vy9ITnlKZGg3NFcvcFFOdEp6SnhITWNSKyJ9fV0sInBsYXRmb3JtIjoic21iYSIsInJvdXRpbmdJbmZvIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoiQ0FVSUVnPT0ifSwibGFzdEFjY291bnRTeW5jVGltZXN0YW1wIjoxNzU2ODcwODkwLCJsYXN0UHJvcEhhc2giOiJQV2s1QiIsIm15QXBwU3RhdGVLZXlJZCI6IkFBQUFBQzJpIn0=",
 // add your Session Id 
 AUTO_STATUS_SEEN: process.env.AUTO_STATUS_SEEN || "true",
 // make true or false status auto seen
@@ -23,9 +23,9 @@ PREFIX: process.env.PREFIX || ".",
 // add your prifix for bot
 MENU_IMAGE_URL: process.env.MENU_IMAGE_URL || "https://ik.imagekit.io/mrshaban/Picsart_25-02-01_22-47-44-239.jpg",
 // add custom menu and mention reply image url   
-BOT_NAME: process.env.BOT_NAME || "SHABAN-MD V5",
+BOT_NAME: process.env.BOT_NAME || "ZAHID-MD V5",
 // add bot namw here for menu
-STICKER_NAME: process.env.STICKER_NAME || "SHABAN-MD",
+STICKER_NAME: process.env.STICKER_NAME || "ZAHID-MD",
 // type sticker pack name 
 CUSTOM_REACT: process.env.CUSTOM_REACT || "false",
 // make this true for custum emoji react    
@@ -33,15 +33,15 @@ CUSTOM_REACT_EMOJIS: process.env.CUSTOM_REACT_EMOJIS || "💝,💖,💗,❤️�
 // chose custom react emojis by yourself 
 DELETE_LINKS: process.env.DELETE_LINKS || "false",
 // automatic delete links witho remove member 
-OWNER_NUMBER: process.env.OWNER_NUMBER || "923059395959",
+OWNER_NUMBER: process.env.OWNER_NUMBER || "923248161773",
 // add your bot owner number
-OWNER_NAME: process.env.OWNER_NAME || "MR-SHABAN",
+OWNER_NAME: process.env.OWNER_NAME || "ZAHID-773-WALE",
 // add bot owner name
 DESCRIPTION: process.env.DESCRIPTION || "*© ᴘᴏᴡᴇʀᴇᴅ ʙʏ Mʀ Sʜᴀʙᴀɴ*",
 // add bot owner name    
 ALIVE_IMG: process.env.ALIVE_IMG || "https://ik.imagekit.io/mrshaban/Picsart_25-02-01_22-47-44-239.jpg",
 // add img for alive msg
-LIVE_MSG: process.env.LIVE_MSG || "> Zinda Hun Yar *SHABAN-MD*⚡",
+LIVE_MSG: process.env.LIVE_MSG || "> Zinda Hun Yar *ZAHID-MD*⚡",
 // add alive msg here 
 READ_MESSAGE: process.env.READ_MESSAGE || "false",
 // Turn true or false for automatic read msgs
@@ -67,7 +67,7 @@ AUTO_TYPING: process.env.AUTO_TYPING || "false",
 // true for automatic show typing   
 READ_CMD: process.env.READ_CMD || "false",
 // true if want mark commands as read 
-DEV: process.env.DEV || "923043788282",
+DEV: process.env.DEV || "923248161773",
 //replace with your whatsapp number        
 ANTI_VV: process.env.ANTI_VV || "false",
 // true for anti once view
